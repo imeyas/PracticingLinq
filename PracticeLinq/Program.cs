59ca218e8fc57d0d070b1aa09aab0e39f8d17e65
Ebo Onwunali <ebo@njgeneral.com>
2022-01-28 15:48:01 -0500

    Finished code for this exercise

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "Game1Seven", "Game2FortyFive", "Game3Five", "Game4Three", "Game5Fifty", "Game6Sixty", "Game7Seventy" };
+
+            var order = list.OrderBy(x => x.Length ).ToList();
+
+            foreach (var item in order)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /*
+         * (Using Method Syntax)
+        Create a list of video game names..
+        Order the list of games by length of the game name.
+        Use the lambda expression in this exercise as well.
+        */
+    }
+}
